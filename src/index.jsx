8cb89a3fbf2ea220e28aacc97dc9a04850a150e6
molinero <98057549+molinero221@users.noreply.github.com>
2023-03-11 14:31:57 +0700

    React Router package installed

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './styling/style.css'
 import { ContextProvider } from './Context/Context'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ContextProvider> 
-      <App />
-    </ContextProvider>
+    <Router>
+      <ContextProvider> 
+        <App />
+      </ContextProvider>
+    </Router>
   </React.StrictMode>,
 )
