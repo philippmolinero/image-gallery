693c3307903a8c90c6bc87326b45888240c12c7f
molinero <98057549+molinero221@users.noreply.github.com>
2023-03-14 15:56:31 +0700

    added message to cart page if cart is empty

--- src/components/Cartitem.jsx
+++ src/components/Cartitem.jsx
@@ -4,23 +4,23 @@ import { useContext } from "react";
 
 const CartItem = () => {
 
-  const {shoppingCart} = useContext(Context)
+  const {shoppingCart, removeFromCart} = useContext(Context)
 
   const cartItems = shoppingCart.map(item => {
     return (
-      <div className="cart-item-container">
-        <i className="fa-solid fa-trash"></i>
+      <div className="cart-item-container" key={item.id}>
+        <i className="fa-solid fa-trash" onClick={() => removeFromCart(item)}></i>
         <img src={item.url} className="cart-img" key={item.id} />
         <p>Price: $6.99</p>
       </div>
       )
   })
 
-
+  console.log(Array.isArray(shoppingCart))
   return (
     <div className="cart-container">
       <h1>You Cart</h1>
-      {cartItems}
+      {shoppingCart.length ? cartItems : <h2>nothing in here here</h2> }
     </div>
   )
 }
